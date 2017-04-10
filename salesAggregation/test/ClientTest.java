d622af491d3b66846fe8a517a9d934f02a8ace1c
adamstom97 <adamstom97@adamstom97-PC>
2017-04-10 19:02:24 +1200

    Finished test client.

--- salesAggregation/test/ClientTest.java
+++ salesAggregation/test/ClientTest.java
@@ -9,12 +9,9 @@ import domain.Sale;
 import domain.SaleItem;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
-import org.junit.After;
 import org.junit.AfterClass;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.ArrayList;
@@ -26,40 +23,31 @@ import remote.ISalesAgg;
  * @author adamstom97
  */
 public class ClientTest {
-    private static Registry registry;
     private static ISalesAgg server;
-    private Sale sale;
     
     public ClientTest() {
     }
     
     @BeforeClass
     public static void setUpClass() throws RemoteException, NotBoundException {
-        registry = LocateRegistry.getRegistry("localhost");
+        Registry registry = LocateRegistry.getRegistry("localhost");
         server = (ISalesAgg) registry.lookup("sales");
     }
     
     @AfterClass
     public static void tearDownClass() {
     }
-    
-    @Before
-    public void setUp() {     
+
+    @Test
+    public void newSaleTest() throws RemoteException {
         SaleItem item = new SaleItem("FW1234", 10.0, 3.5);
         Collection<SaleItem> items = new ArrayList<>();
         items.add(item);
         
         Customer customer = new Customer('M', "27/04/97");
-        sale = new Sale("13/04/17", customer);
+        Sale sale = new Sale("13/04/17", customer);
         sale.setItems(items);
-    }
-    
-    @After
-    public void tearDown() {
-    }
-
-    @Test
-    public void newSaleTest() throws RemoteException {
+        
         server.newSale(sale);
     }
 }
