3587305bdc75616c7e81986ab4df792c4391af1a
adath325 <adath325@mds-xa201s-p064.vdi.student.otago.ac.nz>
2017-04-12 14:39:12 +1200

    Added filters to allow the server to run.

--- loyaltyService/src/server/Server.java
+++ loyaltyService/src/server/Server.java
@@ -5,27 +5,43 @@
  */
 package server;
 
+import filters.CorsFilter;
+import filters.DebugFilter;
 import java.net.URI;
 import java.net.URISyntaxException;
 import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.slf4j.bridge.SLF4JBridgeHandler;
+import resource.CouponResource;
+import resource.CouponsResource;
+import resource.PointsResource;
+import resource.TransactionResource;
+import resource.TransactionsResource;
 
 /**
  * A server for running the Loyalty Service application.
- * 
+ *
  * @author adamstom97
  */
 public class Server {
-    public static void main(String[] args) throws URISyntaxException {
-        SLF4JBridgeHandler.removeHandlersForRootLogger();
-        SLF4JBridgeHandler.install();
-        
-        ResourceConfig config = new ResourceConfig();
-        
-        //config.register
-        
-        URI baseUri = new URI("http://localhost:8081");
-        JdkHttpServerFactory.createHttpServer(baseUri, config);
-    }
+
+	public static void main(String[] args) throws URISyntaxException {
+		SLF4JBridgeHandler.removeHandlersForRootLogger();
+		SLF4JBridgeHandler.install();
+
+		ResourceConfig config = new ResourceConfig();
+
+		config.register(DebugFilter.class);
+		config.register(CorsFilter.class);
+
+		config.register(CouponResource.class);
+		config.register(CouponsResource.class);
+		config.register(PointsResource.class);
+		config.register(TransactionResource.class);
+		config.register(TransactionsResource.class);
+
+		URI baseUri = new URI("http://localhost:8081/");
+		JdkHttpServerFactory.createHttpServer(baseUri, config);
+		System.out.println("Service Ready on " + baseUri);
+	}
 }
