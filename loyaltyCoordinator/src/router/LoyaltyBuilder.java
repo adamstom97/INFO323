ab3f276c5ddcba1158ee7f6e4e862de3f0455bc6
adath325 <adath325@mds-xa201s-p084.vdi.student.otago.ac.nz>
2017-05-13 16:55:18 +1200

    Initial classes for router.

---
+++ loyaltyCoordinator/src/router/LoyaltyBuilder.java
@@ -0,0 +1,15 @@
+package router;
+
+import org.apache.camel.builder.RouteBuilder;
+
+/**
+ *
+ * @author adath325
+ */
+public class LoyaltyBuilder extends RouteBuilder {
+
+	@Override
+	public void configure() {
+		
+	}
+}
---
+++ loyaltyCoordinator/src/router/LoyaltyRouter.java
@@ -0,0 +1,31 @@
+package router;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.camel.CamelContext;
+import org.apache.camel.component.jms.JmsComponent;
+import org.apache.camel.impl.DefaultCamelContext;
+
+/**
+ *
+ * @author adath325
+ */
+public class LoyaltyRouter {
+
+	public static void main(String[] args) throws Exception {
+		CamelContext camel = new DefaultCamelContext();
+
+		ActiveMQConnectionFactory activeMqFactory = new ActiveMQConnectionFactory("tcp://localhost:61616");
+		camel.addComponent("jms", JmsComponent.jmsComponentAutoAcknowledge(activeMqFactory));
+
+		activeMqFactory.setTrustAllPackages(true);
+
+		camel.addRoutes(new LoyaltyBuilder());
+
+		camel.setTracing(false);
+		camel.setStreamCaching(true);
+
+		System.out.println("Starting router...");
+		camel.start();
+		System.out.println("... ready.");
+	}
+}
