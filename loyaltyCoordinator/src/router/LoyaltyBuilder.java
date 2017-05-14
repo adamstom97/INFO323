eef205e1ca0680abb4384141581e1f2b7be7ffba
adath325 <adath325@mds-xa201s-p084.vdi.student.otago.ac.nz>
2017-05-14 14:01:17 +1200

    RMI route from first EIP diagram.

--- loyaltyCoordinator/src/router/LoyaltyBuilder.java
+++ loyaltyCoordinator/src/router/LoyaltyBuilder.java
@@ -1,6 +1,10 @@
 package router;
 
+import domain.Sale;
+import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
 
 /**
  *
@@ -10,6 +14,33 @@ public class LoyaltyBuilder extends RouteBuilder {
 
 	@Override
 	public void configure() {
-		
+		// Customer Makes Purchase
+		from("imaps://outlook.office365.com?username=adath325@student.otago.ac.nz"
+				  + "&password=" + getPassword("Enter your E-Mail password")
+				  + "&searchTerm.subject=Vend:SaleUpdate"
+				  + "&debugMode=false&folderName=Inbox")
+				  .convertBodyTo(String.class).log("${body}")
+				  .to("jms:queue:sale-event");
+		from("jms:queue:sale-event")
+				  .setHeader("id").jsonpath("$.id") 
+				  .setHeader("customerId").jsonpath("$.customer_id") 
+				  .setHeader("price").jsonpath("$.totals.total_price")
+				  .multicast()
+				  .to("jms:queue:new-sale", "jms:queue:new-transaction");
+		from("jms:queue:new-sale").unmarshal().json(JsonLibrary.Gson, Sale.class)
+				  .to("rmi://localhost:1099/sales"
+							 + "?remoteInterfaces=server.ISalesAgg"
+							 + "&method=newSale");
+	}
+
+	public static String getPassword(String prompt) {
+		JPasswordField txtPasswd = new JPasswordField();
+		int resp = JOptionPane.showConfirmDialog(null, txtPasswd, prompt,
+				  JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+		if (resp == JOptionPane.OK_OPTION) {
+			String password = new String(txtPasswd.getPassword());
+			return password;
+		}
+		return null;
 	}
 }
