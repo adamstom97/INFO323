9c07ea4ea4e2fa439accb0e6e79c721efb490708
adath325 <adath325@mds-xa201s-p084.vdi.student.otago.ac.nz>
2017-05-15 19:51:42 +1200

    Beginning work on creating a coupon.

--- loyaltyClient/public_html/scripts/loyalty.js
+++ loyaltyClient/public_html/scripts/loyalty.js
@@ -6,20 +6,57 @@ module.controller('LoyaltyController', function ($scope, $resource) {
 	if (!window.WebSocket) {
 		alert("WebSockets are not supported by this browser");
 	} else {
-		initializeSocket();
+		initializeCustomerSocket();
+		initializeProductSocket();
 	}
-	function initializeSocket() {
-		ws = new WebSocket("ws://localhost:8087/ajax");
-		ws.onmessage = function (event) {
+
+	function initializeCustomerSocket() {
+		ws1 = new WebSocket("ws://localhost:8087/customer");
+		ws1.onmessage = function (event) {
 			var object = JSON.parse(event.data);
 			$scope.customer = object.customers[0];
-			$scope.$apply();			
+
+			var pointsResource = $resource('http://localhost\\:8081/customers/:customer/points/unused', null, {
+				'getPoints': {transformResponse: function (data, headers, status) {
+						return {value: data};
+					}
+				}});
+			pointsResource.getPoints({"customer": $scope.customer.id},
+					  function (points) {
+						  $scope.available = points.value;
+					  });
+
+			$scope.$apply();
 		};
 	}
-	
+
+	function initializeProductSocket() {
+		ws2 = new WebSocket("ws://localhost:8087/product");
+		ws2.onmessage = function (event) {
+
+		};
+	}
+
 	$scope.signIn = function () {
 		var email = document.getElementById("email").value;
-		ws.send(email);
+		ws1.send(email);
+	};
+
+	$scope.updateEquivalent = function () {
+		var wanted = document.getElementById("wanted").value;
+		var equivalent = wanted * 10;
+		document.getElementById("equivalent").innerHTML(equivalent);
+	};
+
+	$scope.createCoupon = function () {
+		var points = document.getElementById("wanted").value; 
+
+		var couponsResource = $resource('http://localhost\\:8081/customers/:customer/coupons', null, {
+			'getPoints': {transformResponse: function (data, headers, status) {
+					return {value: data};
+				}
+			}});
+		couponsResource.save({"customer": $scope.customer.id}, points);
 	};
 }
 );
