f4878d52275eb436a6c8c3faa135ce716689ba59
adath325 <adath325@mds-xa201s-p084.vdi.student.otago.ac.nz>
2017-05-16 18:57:28 +1200

    New coupon is now created, and its generated ID becomes the barcode.

--- loyaltyClient/public_html/scripts/loyalty.js
+++ loyaltyClient/public_html/scripts/loyalty.js
@@ -3,6 +3,7 @@
 var module = angular.module('LoyaltyApp', ['ngResource']);
 module.controller('LoyaltyController', function ($scope, $resource) {
 
+	var serverURI = "http://localhost\\:8081/customers/:customer/";
 	if (!window.WebSocket) {
 		alert("WebSockets are not supported by this browser");
 	} else {
@@ -15,17 +16,20 @@ module.controller('LoyaltyController', function ($scope, $resource) {
 		ws1.onmessage = function (event) {
 			var object = JSON.parse(event.data);
 			$scope.customer = object.customers[0];
-
-			var pointsResource = $resource('http://localhost\\:8081/customers/:customer/points/unused', null, {
+			var pointsResource = $resource(serverURI + 'points/unused', null, {
 				'getPoints': {transformResponse: function (data, headers, status) {
 						return {value: data};
 					}
 				}});
-			pointsResource.getPoints({"customer": $scope.customer.id},
-					  function (points) {
-						  $scope.available = points.value;
-					  });
 
+			try {
+				pointsResource.getPoints({"customer": $scope.customer.id},
+						  function (points) {
+							  $scope.available = points.value;
+						  });
+			} catch (err) {
+				$scope.messages = "That is not a valid email address.";
+			}
 			$scope.$apply();
 		};
 	}
@@ -39,16 +43,43 @@ module.controller('LoyaltyController', function ($scope, $resource) {
 	}
 
 	$scope.signIn = function () {
-		var email = document.getElementById("email").value;
-		ws1.send(email);
+		$scope.messages = null;
+		ws1.send(document.getElementById("email").value);
 	};
 
 	$scope.updateEquivalent = function () {
-		$scope.equivalent = (document.getElementById("wanted").value);
+		var wanted = document.getElementById("wanted").value;
+		if (wanted === "") {
+			wanted = 0;
+		}
+		$scope.equivalent = parseFloat(wanted).toFixed(2);
 	};
 
 	$scope.createCoupon = function () {
-		JsBarcode("#barcode", document.getElementById("equivalent").value);
+		if (parseInt($scope.coupon.points) > parseInt($scope.available) || parseInt($scope.coupon.points) < 0) {
+			$scope.messages = "That is not a valid number of points.";
+		} else {
+			$scope.messages = null;
+			var coupons = $resource(serverURI + 'coupons', null, {create: {method: "POST", headers: {'Content-Type': 'application/json'}}});
+			coupons.create({customer: $scope.customer.id}, $scope.coupon, function (data) {
+				var newCoupon = data;
+
+				var product = new Object();
+				product.handle = newCoupon.od;
+				product.type = "coupon";
+				product.name = "adath325 Coupon";
+				product.retail_price = -(newCoupon.points);
+
+				ws2.send(product);
+
+				JsBarcode('#barcode', newCoupon.id);
+
+				// Clear wanted points and equivalent $ fields
+				$scope.signIn();
+				$scope.coupon.points = null;
+				$scope.equivalent = null;
+			});
+		}
 	};
 }
 );
